67be370a3a991474cadc5c5e8a24c9e83268d55c
shravaniallamreddy <shravani.allam@gmail.com>
2021-08-03 17:35:09 -0600

    update

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+    // const location = useLocation();
+    return (
+        <nav className="navbar navbar-expand-lg justify-content-center navbar-light bg-primary">
+            <h1 className="text-center text-white font-wight-bold">Sample Menu</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
